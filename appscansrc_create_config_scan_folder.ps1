bda09baa76dd309d65a01186393d7a2720eefae1
ctibordudahcl <86057782+ctibordudahcl@users.noreply.github.com>
2023-10-10 13:28:05 +0200

    Update appscansrc_create_config_scan_folder.ps1

--- appscansrc_create_config_scan_folder.ps1
+++ appscansrc_create_config_scan_folder.ps1
@@ -1,27 +1,5 @@
-# Copyright 2023 HCL America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 write-host "======== Step: Creating a config scan folder ========"
 # Creating Appscan Source script file. It is used with AppScanSrcCli to run scans reading folder content and selecting automatically the language (Open Folder command).
-if ($env:compiledArtifactFolder -ne "none"){
-  $content=Get-ChildItem -Path $env:compiledArtifactFolder -filter "*.zip"
-  if ($content){
-    write-host "There is a compiled files compressed in folder $env:compiledArtifactFolder."
-    Expand-Archive -Path $content -DestinationPath $env:compiledArtifactFolder
-  }else{
-    write-host "There is no compiled files compressed."
-  }
   write-output "login" > script.scan
   write-output "of `"$env:WorkingDirectory\$env:compiledArtifactFolder`"" >> script.scan
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`"" >> script.scan
@@ -33,11 +11,20 @@ if ($env:compiledArtifactFolder -ne "none"){
 }
 else{
   write-output "login" > script.scan
-  write-output "of `"$env:WorkingDirectory`"" >> script.scan
-  write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`" -sourcecodeonly true" >> script.scan
+# write-output "of `"$env:WorkingDirectory`"" >> script.scan
+  write-output "RUNAS AUTO" >> script.scan
+  write-output "oa $source -appserver_type $appserver_type -no_ear_project" >> script.scan
   write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$env:aseAppName-$env:BuildNumber.ozasmt`"" >> script.scan
   write-output "exit" >> script.scan
   
   write-host "Config file created (source code only scan)."
 }
+
+    - $env:Path = "C:\Program Files\HCL\AppScanSource\bin;$env:Path"
+    - echo "login_file appscandemo $token -acceptssl" > c:\temp\cli_script_scan_dynamic.script
+    - echo "RUNAS AUTO" >> c:\temp\cli_script_scan_dynamic.script
+    - echo "oa $source -appserver_type $appserver_type -no_ear_project" >> c:\temp\cli_script_scan_dynamic.script
+    - echo "ra "C:\temp\altoromutual.war.ozasmt" -scanconfig "Normal scan"" >> c:\temp\cli_script_scan_dynamic.script
+    - echo "exit" >> c:\temp\cli_script_scan_dynamic.script
+    - AppScanSrcCli.exe script c:\temp\cli_script_scan_dynamic.script
